7aa5a80926fe0bcae0218e421cab75949075d832
Jeel <jeelmalde82000@gmail.com>
2024-08-05 13:07:37 +0530

    auth and token added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const passport = require("./auth");
 const Person = require("./models/Person");
 const MenuItem = require("./models/MenuItem");
 require("dotenv").config();
@@ -8,6 +9,18 @@ const PORT = process.env.PORT || 3000;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+// Middleware Function
+const logRequest = (req, res, next) => {
+	console.log(
+		`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`
+	);
+	next(); // Move on to the next phase
+};
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate("local", { session: false });
 
 const personRoutes = require("./routes/PersonRoutes");
 app.use("/person", personRoutes);
